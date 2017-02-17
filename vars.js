8747c929cff1460db2028a1b2f606f2b7ffc5062
Mark Swedberg <swedy13@gmail.com>
2017-02-16 21:10:09 -0800

    variables

---
+++ vars.js
@@ -0,0 +1,23 @@
+/* var person = {
+ *     name: 'Mark',
+ *     age: 21
+ * };
+ *
+ * function updatePerson(obj) {
+ *     obj.age = 30;
+ * }
+ *
+ * updatePerson(person);
+ * console.log(person);*/
+
+
+// Arrays
+var grades = [15, 88];
+
+function addGrades(grades) {
+    grades.push(55);
+    debugger;
+}
+
+addGrades(grades);
+console.log(grades);
