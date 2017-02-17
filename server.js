fdab8b06d77a83bbdbc32972602e893a4710a786
Mark Swedberg <Mark Swedberg>
2017-02-16 20:02:31 -0800

    Underscore refactor

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -23,28 +24,29 @@ app.get('/todos', function(req, res) {
     res.json(todos);
 });
 app.get('/todos/:id', function(req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var match;
-
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            match = todo;
-        }
-    });
+    var todo = parseInt(req.params.id, 10);
+    var match = _.findWhere(todos, {id: todo});
 
     if (match) {
         res.json(match);
+        return;
     }
-    else {
-        res.status(404).send();
-    }
+    res.status(404).send();
 });
 
 
 // ---- POST METHODS ---- //
 // To Dos
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+    body.description = body.description.trim();
+
+    // Return empty body if the request is bad or empty
+    if (!_.isString(body.description) ||
+        !_.isBoolean(body.completed) ||
+        body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
 
     body.id = todoNextId++;
     todos.push(body);
