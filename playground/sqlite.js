c9979b9bec7dbe4fbaced560fac3841342bb675a
Mark Swedberg <swedy13@gmail.com>
2017-02-18 08:40:30 -0800

    Sqlite

---
+++ playground/sqlite.js
@@ -0,0 +1,55 @@
+// ---- DEPENDENCIES ---- //
+var Sequelize = require('sequelize');
+
+
+// ---- VARIABLES ---- //
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/sqlite-db.sqlite'
+});
+
+
+// ---- TODO ITEMS ---- //
+var Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 250]
+        }
+    },
+    completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+
+// ---- METHODS ---- //
+sequelize.sync().then(function() {
+    console.log('Everything is synced');
+
+    Todo.create({
+        description: 'Slay the dragon',
+    }).then(function() {
+        return Todo.findAll({
+            where: {
+                description: {
+                    $like: '%girl%'
+                }
+            }
+        });
+    }).then(function(todos) {
+        if (todos) {
+            todos.forEach(function(todo) {
+                console.log(todo.toJSON());
+            });
+        }
+        else {
+            console.log('No todo found');
+        }
+    }).catch(function(e) {
+        console.log(e);
+    });
+});
